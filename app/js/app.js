03810466afc96139dd15954f03ad391935ce28eb
Nicolas Molina <nicolas.molina.monroy@gmail.com>
2015-12-02 17:28:20 -0500

    Controller and Databing

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,19 @@
 (function() {
   "use strict";
   var app = angular.module("HelloWorldApp", []);
+
+  app.controller("HelloWorldController",
+    [
+      '$scope',
+      function($scope){
+        $scope.numberA = 0;
+        $scope.numberB = 0;
+        $scope.total = 0;
+
+        $scope.calc = function(){
+          $scope.total = ($scope.numberA + $scope.numberB) * 2;
+        }
+      } 
+    ]
+  );
  }());
